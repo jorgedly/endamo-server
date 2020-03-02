5538e3308ff1045370d0b26cfabd67d1adc7c14f
Jorge Lopez <jorgekl.1994@gmail.com>
2020-03-02 00:27:23 -0600

    Corregidas las funciones

--- web.js
+++ web.js
@@ -5,7 +5,10 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-//app.use(cors());
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 const conn = mysql.createPool({
@@ -15,11 +18,14 @@ const conn = mysql.createPool({
     database: 'heroku_a0e0bff8d195685'
 });
 
-app.get('/users', (req, res) => {
-    let sql = `SELECT * FROM Usuario`;
+app.get('/products', (req, res) => {
+    let sql = `SELECT nombre,precio,cantidad FROM Producto`;
     let query = conn.query(sql, (err, results) => {
-        if (err) throw err;
-        res.send(results);
+        if (err) {
+            res.send([]);
+        } else {
+            res.send(results);
+        }
     });
 });
 
@@ -31,20 +37,20 @@ app.post('/login', (req, res) => {
     let query = conn.query(sql, (err, results) => {
         if (err) throw err;
         if(results.length === 1) {
-            res.send({auth:true, token:123, username, password});
+            res.send({auth:true});
         } else {
-            res.send({auth:false, "username":username,"password": password});
+            res.send({auth:false});
         }
     });
 });
 
 app.post('/register', (req, res) => {
     const datos = req.body;
+    const email = datos.email;
     const username = datos.username;
     const password = datos.password;
-    const email = datos.email;
     const avatar = datos.avatar;
-    let sql = `INSERT INTO Usuario (username, email, password, avatar) VALUES ('${username}','${email}','${password}', '${avatar}')`;
+    let sql = `INSERT INTO Usuario (email, username, password, avatar) VALUES ('${email}','${username}','${password}', '${avatar}')`;
     let query = conn.query(sql, (err, results) => {
         if (err) {
             res.send({'success': false});
@@ -56,10 +62,10 @@ app.post('/register', (req, res) => {
 
 app.post('/registerE', (req, res) => {
     const datos = req.body;
+    const email = datos.email;
     const username = datos.username;
     const password = datos.password;
-    const email = datos.email;
-    let sql = `INSERT INTO Empresa (username, email, password) VALUES ('${username}','${email}','${password}')`;
+    let sql = `INSERT INTO Empresa (email, nombre, password) VALUES ('${email}','${username}','${password}')`;
     let query = conn.query(sql, (err, results) => {
         if (err) {
             res.send({'success': false});
@@ -69,6 +75,4 @@ app.post('/registerE', (req, res) => {
     });
 });
 
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Escuchando en puerto ${port}...`))
