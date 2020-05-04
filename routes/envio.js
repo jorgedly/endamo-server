e03617f0bd65374b56b76ba44c44158e7fd9ed95
Mario Morales <mmario_93@hotmail.es>
2020-05-04 17:49:21 -0600

    Funcionalidad mis pedidos

--- routes/envio.js
+++ routes/envio.js
@@ -16,4 +16,28 @@ router.post('/domiciliio', (req, res) => {
     });
 });
 
+//insertar Entrega
+router.post('/entrega', (req, res) => {
+    const { entregado, usuario_id_usuario, factura_id_factura} = req.body;
+    let sql = `INSERT INTO entrega(entregado, usuario_id_usuario, factura_id_factura) VALUES (${entregado},${usuario_id_usuario}, ${factura_id_factura})`;
+    let query = conn.query(sql, (err, results) => {
+        if (err) {
+            res.send({'success': 0});
+        } else {
+            res.send({'success': 1});
+        }
+    });
+});
+
+//ver mis Entregas
+router.get('/entrega/:id', (req, res) => {
+    const id = req.params.id;
+    let sql = `SELECT * FROM entrega where usuario_id_usuario = '${id}'`;
+    let query = conn.query(sql, (err, results) => {
+        if (err) throw err;
+        console.log(results);
+        res.send(results);
+    });
+});
+
 module.exports = router;
